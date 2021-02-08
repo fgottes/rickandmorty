9f6646f508582cdbc718fb4ae6aa5d3abab6d7cb
fgottes <f.gottesmann@outlook.com>
2021-02-08 16:02:00 +0100

    typo

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,35 +1,28 @@
 import { createElement } from "../../utils/createElements";
 
-export function createCard ({imgSrc, name, species, origin}) {
-    return createElement("div", {
-        className: "card",
-        children: [
-            createElement("img", {
-                className: "card_portrait",
-                src: imgSrc,
+export function createCard({ imgSrc, name, species, origin }) {
+  return createElement("div", {
+    className: "card",
+    children: [
+      createElement("img", {
+        className: "card_portrait",
+        src: imgSrc,
+      }),
 
+      createElement("h2", {
+        className: "card_name",
+        innerText: name,
+      }),
 
-            }),
+      createElement("p", {
+        className: "card_species",
+        innerText: species,
+      }),
 
-            createElement("h2",{
-                className:"card_name",
-                innerText: name,
-
-            }),
-
-            createElement("p"{
-                className: "card_species",
-                innerText: species,
-
-            }),
-
-            createElement("p"{
-                className: "card_origin",
-                innerText: origin,
-
-            }),
-
-
-        ]
-    });
+      createElement("p", {
+        className: "card_origin",
+        innerText: origin,
+      }),
+    ],
+  });
 }

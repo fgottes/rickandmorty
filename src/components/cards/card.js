d764bbdbf72bc5b8ddba0a56f06aa71c7e29f7c1
fgottes <f.gottesmann@outlook.com>
2021-02-08 15:58:31 +0100

    add important files

---
+++ src/components/cards/card.js
@@ -0,0 +1,35 @@
+import { createElement } from "../../utils/createElements";
+
+export function createCard ({imgSrc, name, species, origin}) {
+    return createElement("div", {
+        className: "card",
+        children: [
+            createElement("img", {
+                className: "card_portrait",
+                src: imgSrc,
+
+
+            }),
+
+            createElement("h2",{
+                className:"card_name",
+                innerText: name,
+
+            }),
+
+            createElement("p"{
+                className: "card_species",
+                innerText: species,
+
+            }),
+
+            createElement("p"{
+                className: "card_origin",
+                innerText: origin,
+
+            }),
+
+
+        ]
+    });
+}

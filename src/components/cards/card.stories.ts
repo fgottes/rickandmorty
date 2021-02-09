7f514260a8ea0a3c0723e5e795d500f09b7241cf
fgottes <f.gottesmann@outlook.com>
2021-02-09 17:23:04 +0100

    add array map

--- src/components/cards/card.stories.ts
+++ src/components/cards/card.stories.ts
@@ -1,36 +1,38 @@
 import "./card.css";
 import { createCard } from "./card";
 import { createElement } from "../../utils/createElements";
+import { getCharacter, getCharacters } from "../../utils/api";
+
 
 export default {
   title: "Components/Card",
   parameters: { layout: "centered" },
 };
 
-export const Rick = () =>
-  createCard({
-    imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    name: "Rick Sanchenz",
-    status: "Alive",
-    species: "Human",
-    origin: { name: "Earth (C-137)" },
-  });
-export const Morty = () =>
-  createCard({
-    imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-    name: "Morty Smith",
-    status: "Alive",
-    species: "Human",
-    origin: { name: "Earth (C-137)" },
-  });
-  export const CrabSpider = () =>
-  createCard({
-    imgSrc: "https://rickandmortyapi.com/api/character/avatar/79.jpeg",
-  name: "Crab Spider",
-  status: "Alive",
-  species: "Alien",
-  origin: { name: "Hideout Planet"},
-  });
+// export const Rick = () =>
+//   createCard({
+//     imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+//     name: "Rick Sanchenz",
+//     status: "Alive",
+//     species: "Human",
+//     origin: { name: "Earth (C-137)" },
+//   });
+// export const Morty = () =>
+//   createCard({
+//     imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+//     name: "Morty Smith",
+//     status: "Alive",
+//     species: "Human",
+//     origin: { name: "Earth (C-137)" },
+//   });
+//   export const CrabSpider = () =>
+//   createCard({
+//     imgSrc: "https://rickandmortyapi.com/api/character/avatar/79.jpeg",
+//   name: "Crab Spider",
+//   status: "Alive",
+//   species: "Alien",
+//   origin: { name: "Hideout Planet"},
+//   });
 
   //________________________________________________________________________________________________________________________________________
 
@@ -73,3 +75,30 @@ export const Multiple = () => {
   return container;
   
 };
+
+//_________________________________________________________________________________________________________________________________________
+
+export const CharacterFromAPI = (args, {loaded: { character} }) => {
+  return createCard(character);
+  
+};
+
+CharacterFromAPI.loaders = [
+  async () => ({
+    character: await getCharacter(7),
+  }),
+];
+
+export const CharactersFromAPI = (args, {loaded: { characters } }) => {
+  const container = createElement("article",{
+    className: "container",
+    childs: characters.map((character) => createCard(character)),
+  });
+  return container;
+};
+
+CharactersFromAPI.loaders = [
+  async () => ({
+    characters: await getCharacters(),
+  }),
+];
